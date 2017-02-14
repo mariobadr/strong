7d83a499876dd7c31b3969d61986ab9d65eab623
Mario Badr <mario.badr@outlook.com>
2017-02-14 15:26:36 -0500

    Add inverse of frequency type

--- examples/computer_architecture.cpp
+++ examples/computer_architecture.cpp
@@ -18,10 +18,22 @@ struct frequency : st::type<frequency, double> {
   using type::type;
 };
 
+// Create a type for periods (inverse of frequency)
+struct period : st::type<period, double> {
+  using type::type;
+};
+
+// Calculate the period given a frequency
+constexpr period inverse(frequency const & hertz)
+{
+  return period(1.0 / static_cast<double>(hertz));
+}
+
 int main() {
   cycle_count cycles(50);
   instruction_count instructions(10000);
   frequency clock_rate(2.6);
+  period p = inverse(clock_rate);
 
   return 0;
 }
