59038c387ff1799ae6dd6dab47502ccae81b92b2
Mario Badr <mario.badr@outlook.com>
2017-05-24 14:19:35 -0400

    Add option to build (or not) the examples

--- options.cmake
+++ options.cmake
@@ -5,13 +5,23 @@ option(
 )
 
 option(
+  STRONG_BUILD_EXAMPLES
+  "Build the example executables that use strong"
+  OFF
+)
+
+option(
   STRONG_USE_STL_STREAMS
   "Include and use the std::ostream and std::istream for stream operations"
   ON
 )
 
 if(STRONG_BUILD_DOCS)
-  message(STATUS "strong: Documentation option enabled.")
+  message(STATUS "strong: doc target builds documentation.")
+endif()
+
+if(STRONG_BUILD_EXAMPLES)
+  message(STATUS "strong: Example executables will be built.")
 endif()
 
 if(STRONG_USE_STL_STREAMS)
